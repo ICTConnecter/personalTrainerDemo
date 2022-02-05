f5a6559d52e7292e8c28806d8369e7d7e1d7be20
s14333591 <ictconnecter.m.arakaki@gmail.com>
2022-02-06 03:13:49 +0900

    a

--- routes/bot.js
+++ routes/bot.js
@@ -37,144 +37,196 @@ var lineBot = (req,res) => {
 
 // フォロー時の処理
 
+// var greeting_follow = async (ev) => {
+//     var profile = await client.getProfile(ev.source.userId);
+//     return client.replyMessage(ev.replyToken,[
+//       {
+//         "type": "bubble",
+//         "hero": {
+//           "type": "image",
+//           "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+//           "size": "full",
+//           "aspectRatio": "20:13",
+//           "aspectMode": "cover",
+//           "action": {
+//             "type": "uri",
+//             "uri": "http://linecorp.com/"
+//           }
+//         },
+//         "body": {
+//           "type": "box",
+//           "layout": "vertical",
+//           "contents": [
+//             {
+//               "type": "text",
+//               "text": "自己紹介",
+//               "weight": "bold",
+//               "size": "xl"
+//             },
+//             {
+//               "type": "box",
+//               "layout": "vertical",
+//               "margin": "lg",
+//               "spacing": "sm",
+//               "contents": [
+//                 {
+//                   "type": "box",
+//                   "layout": "baseline",
+//                   "spacing": "sm",
+//                   "contents": [
+//                     {
+//                       "type": "text",
+//                       "text": "フォローありがとうございます。\n JOY FIT 認定トレーナーの野村 修平です。\n ～　自己紹介文　～\n トレーニングについてご不明な点がございましたらご気軽にお声がけください!!",
+//                       "wrap": true,
+//                       "color": "#666666",
+//                       "size": "sm",
+//                       "flex": 5
+//                     }
+//                   ]
+//                 }
+//               ]
+//             }
+//           ]
+//         }
+//       },
+//       {
+//         "type": "bubble",
+//         "hero": {
+//           "type": "image",
+//           "url": "https://personal-trainer-demo.herokuapp.com/images/beforeAfter.jpg",
+//           "size": "full",
+//           "aspectRatio": "20:13",
+//           "aspectMode": "cover",
+//           "action": {
+//             "type": "uri",
+//             "uri": "http://linecorp.com/"
+//           }
+//         },
+//         "body": {
+//           "type": "box",
+//           "layout": "vertical",
+//           "contents": [
+//             {
+//               "type": "text",
+//               "text": "パーソナルトレーニングの\nご案内",
+//               "weight": "bold",
+//               "size": "md"
+//             },
+//             {
+//               "type": "box",
+//               "layout": "vertical",
+//               "margin": "lg",
+//               "spacing": "sm",
+//               "contents": [
+//                 {
+//                   "type": "box",
+//                   "layout": "baseline",
+//                   "spacing": "sm",
+//                   "contents": [
+//                     {
+//                       "type": "text",
+//                       "text": "ダイエットやバルクアップのためのトレーニング、食事に関するご相談を承っております。\nパーソナルトレーニングでダイエットを2か月実施したトレーニーのBefore Afterを公開しております。\nご興味のある方は下の「Before After」より詳細をご確認ください。\n初回の無料相談もございますので、ご気軽に下の「パーソナルトレーニング受付」よりご連絡ください。",
+//                       "wrap": true,
+//                       "color": "#666666",
+//                       "size": "sm",
+//                       "flex": 5
+//                     }
+//                   ]
+//                 }
+//               ]
+//             }
+//           ]
+//         },
+//         "footer": {
+//           "type": "box",
+//           "layout": "vertical",
+//           "spacing": "sm",
+//           "contents": [
+//             {
+//               "type": "button",
+//               "style": "link",
+//               "height": "sm",
+//               "action": {
+//                 "type": "uri",
+//                 "label": "Before After",
+//                 "uri": "https://line.me/R/home/public/post?id=483iqyqo&postId=1164408225807023777"
+//               }
+//             },
+//             {
+//               "type": "button",
+//               "style": "link",
+//               "height": "sm",
+//               "action": {
+//                 "type": "uri",
+//                 "label": "パーソナルトレーニング受付",
+//                 "uri": "https://personal-trainer-demo.herokuapp.com/liff"
+//               }
+//             },
+//             {
+//               "type": "spacer",
+//               "size": "sm"
+//             }
+//           ],
+//           "flex": 0
+//         }
+//       }
+//       ]);
+// }
+
+
 var greeting_follow = async (ev) => {
-    var profile = await client.getProfile(ev.source.userId);
-    return client.replyMessage(ev.replyToken,
-      // [
-      {
-        "type": "bubble",
-        "hero": {
-          "type": "image",
-          "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-          "size": "full",
-          "aspectRatio": "20:13",
-          "aspectMode": "cover",
-          "action": {
-            "type": "uri",
-            "uri": "http://linecorp.com/"
-          }
-        },
-        "body": {
-          "type": "box",
-          "layout": "vertical",
-          "contents": [
-            {
-              "type": "text",
-              "text": "自己紹介",
-              "weight": "bold",
-              "size": "xl"
-            },
-            {
-              "type": "box",
-              "layout": "vertical",
-              "margin": "lg",
-              "spacing": "sm",
-              "contents": [
-                {
-                  "type": "box",
-                  "layout": "baseline",
-                  "spacing": "sm",
-                  "contents": [
-                    {
-                      "type": "text",
-                      "text": "フォローありがとうございます。\n JOY FIT 認定トレーナーの野村 修平です。\n ～　自己紹介文　～\n トレーニングについてご不明な点がございましたらご気軽にお声がけください!!",
-                      "wrap": true,
-                      "color": "#666666",
-                      "size": "sm",
-                      "flex": 5
-                    }
-                  ]
-                }
-              ]
-            }
-          ]
+  var profile = await client.getProfile(ev.source.userId);
+  return client.replyMessage(ev.replyToken,{
+      "type": "bubble",
+      "hero": {
+        "type": "image",
+        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+        "size": "full",
+        "aspectRatio": "20:13",
+        "aspectMode": "cover",
+        "action": {
+          "type": "uri",
+          "uri": "http://linecorp.com/"
         }
+      },
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "text",
+            "text": "自己紹介",
+            "weight": "bold",
+            "size": "xl"
+          },
+          {
+            "type": "box",
+            "layout": "vertical",
+            "margin": "lg",
+            "spacing": "sm",
+            "contents": [
+              {
+                "type": "box",
+                "layout": "baseline",
+                "spacing": "sm",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "フォローありがとうございます。\n JOY FIT 認定トレーナーの野村 修平です。\n ～　自己紹介文　～\n トレーニングについてご不明な点がございましたらご気軽にお声がけください!!",
+                    "wrap": true,
+                    "color": "#666666",
+                    "size": "sm",
+                    "flex": 5
+                  }
+                ]
+              }
+            ]
+          }
+        ]
       }
-      // ,
-      // {
-      //   "type": "bubble",
-      //   "hero": {
-      //     "type": "image",
-      //     "url": "https://personal-trainer-demo.herokuapp.com/images/beforeAfter.jpg",
-      //     "size": "full",
-      //     "aspectRatio": "20:13",
-      //     "aspectMode": "cover",
-      //     "action": {
-      //       "type": "uri",
-      //       "uri": "http://linecorp.com/"
-      //     }
-      //   },
-      //   "body": {
-      //     "type": "box",
-      //     "layout": "vertical",
-      //     "contents": [
-      //       {
-      //         "type": "text",
-      //         "text": "パーソナルトレーニングの\nご案内",
-      //         "weight": "bold",
-      //         "size": "md"
-      //       },
-      //       {
-      //         "type": "box",
-      //         "layout": "vertical",
-      //         "margin": "lg",
-      //         "spacing": "sm",
-      //         "contents": [
-      //           {
-      //             "type": "box",
-      //             "layout": "baseline",
-      //             "spacing": "sm",
-      //             "contents": [
-      //               {
-      //                 "type": "text",
-      //                 "text": "ダイエットやバルクアップのためのトレーニング、食事に関するご相談を承っております。\nパーソナルトレーニングでダイエットを2か月実施したトレーニーのBefore Afterを公開しております。\nご興味のある方は下の「Before After」より詳細をご確認ください。\n初回の無料相談もございますので、ご気軽に下の「パーソナルトレーニング受付」よりご連絡ください。",
-      //                 "wrap": true,
-      //                 "color": "#666666",
-      //                 "size": "sm",
-      //                 "flex": 5
-      //               }
-      //             ]
-      //           }
-      //         ]
-      //       }
-      //     ]
-      //   },
-      //   "footer": {
-      //     "type": "box",
-      //     "layout": "vertical",
-      //     "spacing": "sm",
-      //     "contents": [
-      //       {
-      //         "type": "button",
-      //         "style": "link",
-      //         "height": "sm",
-      //         "action": {
-      //           "type": "uri",
-      //           "label": "Before After",
-      //           "uri": "https://line.me/R/home/public/post?id=483iqyqo&postId=1164408225807023777"
-      //         }
-      //       },
-      //       {
-      //         "type": "button",
-      //         "style": "link",
-      //         "height": "sm",
-      //         "action": {
-      //           "type": "uri",
-      //           "label": "パーソナルトレーニング受付",
-      //           "uri": "https://personal-trainer-demo.herokuapp.com/liff"
-      //         }
-      //       },
-      //       {
-      //         "type": "spacer",
-      //         "size": "sm"
-      //       }
-      //     ],
-      //     "flex": 0
-      //   }
-      // }
-      // ]
-      );
+    });
 }
+
 
 // メッセージ受信時の処理
 
