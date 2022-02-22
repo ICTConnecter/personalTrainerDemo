4c59e0b88a17ec5a71cd258a8cac72d3d39b6b08
s14333591 <ictconnecter.m.arakaki@gmail.com>
2022-02-22 23:48:31 +0900

    a

--- routes/bot.js
+++ routes/bot.js
@@ -185,7 +185,7 @@ var handleMessageEvent = async (ev) => {
     var profile = await client.getProfile(ev.source.userId);
     var text = (ev.message.type === 'text') ? ev.message.text : '';
     
-    if ('診断開始' = text) {
+    if ('診断開始' == text) {
       return client.replyMessage(ev.replyToken,[{
         "type":"text",
         "text":`性別を選択してください`
