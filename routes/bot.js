c5593cf2bc5b9f8a90c79e8395d1f3b7d2c756fb
s14333591 <ictconnecter.m.arakaki@gmail.com>
2022-02-23 23:36:30 +0900

    a

--- routes/bot.js
+++ routes/bot.js
@@ -662,7 +662,7 @@ var handleMessageEvent = async (ev) => {
 // imagemapメッセージ用画像レスポンス
 router.get('/imagemap/chooseSex/700', function (req, res) {
   console.log("通りました");
-  let filepath = path.join(__dirname, 'public/images/imagemap/chooseSex/700');
+  let filepath = path.join(__dirname, 'public/images/imagemap/chooseSex/700.jpg');
   res.sendFile(filepath);
 });
 router.get('/imagemap/chooseMensBody/:filename', function (req, res) {
